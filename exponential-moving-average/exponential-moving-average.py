3ed5bf5e02d0fe479961ab076049e848d17f54fc
Priyadip Sau <saupriyadip571@gmail.com>
2026-09-22 02:35:40 +0530

    Solve exponential-moving-average

--- exponential-moving-average/exponential-moving-average.py
+++ exponential-moving-average/exponential-moving-average.py
@@ -1,6 +1,6 @@
 def exponential_moving_average(values: list, alpha: float) -> list:
     """ Returns the exponential moving average at every position. """
-    ans = [values[0]]
+    ans = [va
     for i in range(1,len(values)):
         ans.append(alpha*values[i] + (1-alpha)*ans[i-1])
     return ans
